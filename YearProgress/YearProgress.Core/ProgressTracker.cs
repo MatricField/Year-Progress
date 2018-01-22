252a535cc68848e0e4bfbf86332c322939a9b47e
MatricField <david.xelnaga@gmail.com>
2018-01-22 12:20:11 -0800

    Prototype

---
+++ YearProgress/YearProgress.Core/ProgressTracker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace YearProgress.Core
+{
+    public class ProgressTracker
+    {
+        public ProgressTracker()
+        {
+            var now = DateTime.Now;
+            var fullProgressSpan = new DateTime(now.Year, 12, 31, 23, 59, 59) - new DateTime(now.Year, 1, 1);
+            CompleteProgressValue = Convert.ToDouble(fullProgressSpan.Ticks);
+            UpdateYearProgress();
+        }
+
+        protected ProgressTracker(NoOpConstructorFlag dummy)
+        {
+
+        }
+
+        /// <summary>
+        /// The numerator of
+        /// the ratio representing the current progress.
+        /// </summary>
+        /// <seealso cref="CompleteProgressValue"/>
+        public double CurrentProgressValue { get; protected set; }
+
+        /// <summary>
+        /// The denominator of
+        /// the ratio representing the current progress.
+        /// </summary>
+        /// <seealso cref="CurrentProgressValue"/>
+        public double CompleteProgressValue { get; protected set; }
+
+        public virtual void UpdateYearProgress()
+        {
+            var now = DateTime.Now;
+            var elapsedTime = now - new DateTime(now.Year, 1, 1);
+            var newProgressValue = Convert.ToDouble(elapsedTime.Ticks);
+            CurrentProgressValue = newProgressValue;
+        }
+
+        protected struct NoOpConstructorFlag { }
+    }
+}
---
+++ YearProgress/YearProgress.UWP/ViewModel/ProcessTrackerViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YearProgress.Core;
+
+namespace YearProgress.UWP.ViewModel
+{
+    public class ProgressTrackerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ProgressTracker Model { get; }
+
+        public int Minimum { get; } = 0;
+
+        private int _Maximum;
+
+        public int Maximum
+        {
+            get => _Maximum;
+            set
+            {
+                Interlocked.Exchange(ref _Maximum, value);
+                OnPropertyChanged(nameof(Maximum));
+            }
+        }
+
+        private int _Value;
+
+        public int Value
+        {
+            get => _Value;
+            set
+            {
+                Interlocked.Exchange(ref _Value, value);
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+
+        public ProgressTrackerViewModel()
+            : this(new ProgressTracker())
+        {
+        }
+
+        public ProgressTrackerViewModel(ProgressTracker model)
+        {
+            Model = model;
+            Recalculate();
+        }
+
+        public void UpdateProgress()
+        {
+            Model.UpdateYearProgress();
+            Recalculate();
+        }
+
+        protected virtual void Recalculate()
+        {
+            var now = DateTime.Now;
+            var yearSpan = new DateTime(now.Year, 12, 31) - new DateTime(now.Year, 1, 1);
+            Maximum = yearSpan.Days;
+            Value =
+                Convert.ToInt32(
+                    (Model.CurrentProgressValue / Model.CompleteProgressValue) * Maximum
+                    );
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
